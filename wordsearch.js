038c7d1e361b3ce63337916c86670893b536bfd9
karanOO89 <gk.987680@gmail.com>
2021-04-08 00:21:14 +0000

    pairprogramming test was success ..Hurraygit status!

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//from yesterday
+const transpose = function (matrix) {
+  let finalArray = [];
+  let holderArray = [];
+  let count = 0;
 
-module.exports = wordSearch
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (const elm of matrix) {
+      holderArray.push(elm[count]);
+    }
+    finalArray.push(holderArray);
+    count += 1;
+    holderArray = [];
+  }
+
+  return finalArray;
+};
+
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return "You didn't enter anything.";
+  }
+  const transposedLetters = transpose(letters);
+  const horizontalJoin = letters.map((element) => element.join(""));
+  const verticalJoin = transposedLetters.map((element) => element.join(""));
+
+  for (let row of horizontalJoin) {
+    if (row.includes(word)) {
+      return true;
+    }
+  }
+  for (let row of verticalJoin) {
+    if (row.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
